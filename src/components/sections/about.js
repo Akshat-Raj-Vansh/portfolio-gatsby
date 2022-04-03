08058dbcdf9a8405cfc55b2200d3bb08372dc543
arveus <48393979+Akshat-Raj-Vansh@users.noreply.github.com>
2022-04-03 13:43:58 +0530

    edited About Me

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -126,7 +126,7 @@ const About = () => {
     sr.reveal(revealContainer.current, srConfig());
   }, []);
 
-  const skills = ['JavaScript (ES6+)', 'TypeScript', 'React', 'Eleventy', 'Node.js', 'WordPress'];
+  const skills = ['Dart', 'BlockChain', 'Flutter', 'Android', 'TypeScript', 'Node.js', 'AWS'];
 
   return (
     <StyledAboutSection id="about" ref={revealContainer}>
@@ -136,30 +136,26 @@ const About = () => {
         <StyledText>
           <div>
             <p>
-              Hello! My name is Akshat Raj Vansh and I enjoy creating things that live on the internet. My
-              interest in web development started back in 2012 when I decided to try editing custom
-              Tumblr themes — turns out hacking together a custom reblog button taught me a lot
-              about HTML &amp; CSS!
+              Hello! My name is Akshat Raj Vansh and I am currently pursuing my Bachelors of Computer Science and Engineering (Dual Degree)
+               at National Institute of Technology, Hamirpur. I am a self-taught developer and I am passionate about building mobile applications and web applications.
+
             </p>
 
             <p>
-              Fast-forward to today, and I’ve had the privilege of working at{' '}
-              <a href="https://us.mullenlowe.com/">an advertising agency</a>,{' '}
-              <a href="https://starry.com/">a start-up</a>,{' '}
-              <a href="https://www.apple.com/">a huge corporation</a>, and{' '}
-              <a href="https://scout.camd.northeastern.edu/">a student-led design studio</a>. My
-              main focus these days is building accessible, inclusive products and digital
-              experiences at <a href="https://upstatement.com/">Upstatement</a> for a variety of
-              clients.
+              Fast-forward to today, and I’ve had the privilege of organising and mentoring at{' '}
+              <a href="https://iitk.ac.in/new/data/iitk/">Summer Workshop @IITK on IoT and Machine Learning</a>,{' '},
+              working at IITK based startup 
+              <a href="https://ismrititek.com/">ISmriti</a>,{' '}
+              <a href="https://www.apple.com/">a huge corporation</a>. My
+              main focus these days is building apps based on Blockchain Technology and participating in the Hackathons.
             </p>
 
             <p>
               I also recently{' '}
-              <a href="https://www.newline.co/courses/build-a-spotify-connected-app">
-                launched a course
+              <a href="https://github.com/Akshat-Raj-Vansh/ccareV2_frontend/tree/dev">
+                launched an app
               </a>{' '}
-              that covers everything you need to build a web app with the Spotify API using Node
-              &amp; React.
+              that helps users to get emergency help from the nearest hospital and much more.
             </p>
 
             <p>Here are a few technologies I’ve been working with recently:</p>
