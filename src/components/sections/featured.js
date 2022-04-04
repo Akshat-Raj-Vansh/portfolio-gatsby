36e470a4df9c9d96123dc3ec0529fef62b0573a0
arveus <48393979+Akshat-Raj-Vansh@users.noreply.github.com>
2022-04-04 18:24:55 +0530

    fixed color scheme

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -157,7 +157,7 @@ const StyledProject = styled.li`
     z-index: 2;
     padding: 25px;
     border-radius: var(--border-radius);
-    background-color: var(--light-navy);
+    background-color: var(--grey-light);
     color: var(--light-slate);
     font-size: var(--fz-lg);
 
