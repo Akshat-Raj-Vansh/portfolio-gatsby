eba4b698aa6c54f27358a260150b3efe36fdd8a1
arveus <48393979+Akshat-Raj-Vansh@users.noreply.github.com>
2022-04-04 14:11:15 +0530

    changed the color scheme and github repo

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -53,7 +53,7 @@ const GlobalStyle = createGlobalStyle `
   /* Scrollbar Styles */
   html {
     scrollbar-width: thin;
-    scrollbar-color: var(--dark-slate) var(--navy);
+    scrollbar-color: var(--dark-slate) var(--dark-navy);
   }
   body::-webkit-scrollbar {
     width: 12px;
