6fa3b10ba6d16ebfdf0a0fd1de3b1603c15fad23
arveus <48393979+Akshat-Raj-Vansh@users.noreply.github.com>
2022-04-08 09:44:45 +0530

    removed sections and added image

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -171,7 +171,7 @@ const About = () => {
           <div className="wrapper">
             <StaticImage
               className="img"
-              src="../../images/arv.jpg"
+              src="../../images/akshat.jpg"
               width={500}
               quality={95}
               formats={['AUTO', 'WEBP', 'AVIF']}
